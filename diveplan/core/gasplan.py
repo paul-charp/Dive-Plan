e68e69b2bfe259182894c6baccd495e35151480e
Paul Charpentier <119979412+paul-charp@users.noreply.github.com>
2025-02-11 18:30:43 +0100

    Fix gas switch increment

--- diveplan/core/gasplan.py
+++ diveplan/core/gasplan.py
@@ -41,22 +41,26 @@ class GasPlan:
         return Gas(round(frac_O2, 2), round(frac_He, 2))
 
     def getNextGasSwitch(
-        self, P_amb: Pressure, current_gas: Gas, P_surf: Pressure = constants.P_ATM
+        self,
+        P_amb: Pressure,
+        current_gas: Gas,
+        P_surf: Pressure = constants.P_ATM,
+        stop_inc: float = constants.STOP_INC,
     ) -> list[tuple[Pressure, Gas]]:
 
         best_gas: Gas = None
 
         gas_switches: list[tuple[Pressure, Gas]] = []
 
-        while P_amb > P_surf:
+        P_amb = P_amb.round_to_shallower_depth_inc()
 
-            P_amb = P_amb.round_to_shallower_depth_inc()
+        while P_amb > P_surf:
             best_gas = self.bestGases(P_amb)[0]
 
             if best_gas != current_gas:
                 gas_switches.append((P_amb, best_gas))
                 current_gas = best_gas
 
-            P_amb -= 0.1  # Dynamicaly define this from depth increment. Or make a method 'next depth inc'
+            P_amb = Pressure.from_depth(P_amb.to_depth() - stop_inc)
 
         return gas_switches
