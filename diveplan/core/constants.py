b2718cb0afe8df20ceeaf440c177a90d5afe7a83
Paul Charpentier <119979412+paul-charp@users.noreply.github.com>
2025-02-11 17:26:48 +0100

    Refactor Constants

--- diveplan/core/constants.py
+++ diveplan/core/constants.py
@@ -1,3 +1,5 @@
+from diveplan.core.pressure import Pressure
+
 # SURFACE AIR COMPOSITION
 AIR_FO2 = 0.21
 AIR_FN2 = 0.79
@@ -6,17 +8,28 @@ AIR_FHE = 0
 # PHYSICS
 G = 9.81
 
-# Algorithms Floats Precisions
+# Algorithms Precisions
 P_PRECISION = 5
 DEPTH_PRECISION = 2
 
+SAMPLE_RATE = 0.1
+
 # TEMP DEFAULT VALUES
 WATER_DENSITY = 1020
-P_ATM = 1.01325
+P_ATM = Pressure(1.01325)
 
+# DIVE PLANING DEFAULT VALUES
 ASC_RATE = 10  # m/min
 DES_RATE = 20  # m/min
+
+MIN_PPO2: Pressure = Pressure(0.18)
+DECO_PP02: Pressure = Pressure(1.61362)
+BOT_PPO2: Pressure = Pressure(1.4)
+
 STOP_INC = 3
 LAST_STOP = 6
-MIN_PPO2 = 0.18
-DECO_PP02 = 1.6
+
+SWITCH_AT_STOPS = False
+
+DECO_SAC = 15
+BOT_SAC = 20
