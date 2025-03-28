5a89cd042f4b00daa8f93ed344ab7584f62212f7
Paul Charpentier <119979412+paul-charp@users.noreply.github.com>
2025-03-28 14:16:52 +0100

    Simplify consume_gases, Fixes

--- diveplan/core/gasplan.py
+++ diveplan/core/gasplan.py
@@ -7,7 +7,6 @@ from diveplan.core.pressure import Pressure
 
 
 class GasPlan:
-
     def __init__(self, gases: list[Gas]):
         super(GasPlan, self).__init__()
 
@@ -22,15 +21,19 @@ class GasPlan:
         self.otu: float = 0
         self.cns: float = 0
 
-    def bestGases(self, P_amb: Pressure) -> list[Gas]:
+    def best_gases(self, P_amb: Pressure, enforce_max_ppN2: bool = True) -> list[Gas]:
+        """
+        Get the best gases for the given ambient pressure.
 
-        best_gases: list[Gas] = []
+        Arguments:
+            P_amb -- _description_
 
-        for gas in self.gases:
-            if (gas.maxOperatingPressure() >= P_amb) and (
-                gas.ppO2(P_amb) >= constants.MIN_PPO2
-            ):
-                best_gases.append(gas)
+        Returns:
+            _description_
+        """
+        best_gases: list[Gas] = [
+            gas for gas in self.gases if gas.is_breathable(P_amb, enforce_max_ppN2)
+        ]
 
         best_gases.sort(
             key=lambda gas: (gas.ppO2(P_amb), gas.ppHe(P_amb)), reverse=True
@@ -38,7 +41,7 @@ class GasPlan:
 
         return best_gases
 
-    def getNextGasSwitch(
+    def get_next_gas_switches(
         self,
         P_amb: Pressure,
         current_gas: Gas,
@@ -59,7 +62,7 @@ class GasPlan:
         P_amb = P_amb.round_to_shallower_depth_inc()
 
         while P_amb > P_surf:
-            best_gas = self.bestGases(P_amb)[0]
+            best_gas = self.best_gases(P_amb)[0]
 
             if best_gas != current_gas:
                 gas_switches.append((P_amb, best_gas))
@@ -69,43 +72,27 @@ class GasPlan:
 
         return gas_switches
 
-    @staticmethod
-    def TI(t: float, pO2: float, c: float):
-        return (t**2) * (float(pO2) ** c)
+    def get_matching_gases(self, gas: Gas) -> list[Gas]:
+        """
+        Get all gases that match the given gas."
+        """
+        matching_gases: list[Gas] = []
 
-    def consume_gases(self, divestep: DiveStep):
+        for g in self.gases:
+            if g == gas:
+                matching_gases.append(g)
 
-        try:
-            breathing_gas: Gas = next(
-                (gas for gas in self.gases if divestep.gas == gas)
-            )
+        return matching_gases
 
-        except StopIteration:
-            self.gases.append(breathing_gas)
-            breathing_gas: Gas = divestep.gas
+    def consume_gases(self, divestep: DiveStep) -> None:
 
-        if breathing_gas in self.gases:
-            # Get the gas
-            pass
+        gas: Gas = divestep.gas
 
-        else:
-            self.gases.append(breathing_gas)
+        if gas not in self.gases:
+            self.gases.append(gas)
 
-        for gas in self.gases:
-            if gas == divestep.gas:
-                breathing_gas = gas
-                break
+        depth: float = divestep.average_depth
+        time: float = divestep.time
 
-        if breathing_gas is None:
-            breathing_gas = divestep.gas
-            self.gases.append(breathing_gas)
-
-        depth = divestep.average_depth
-        time = divestep.time
-
-        P_amb = Pressure.from_depth(depth)
-        breathing_gas.consume(P_amb, time)
-
-        ppO2 = breathing_gas.ppO2(P_amb)
-        self.otu += self.TI((time / 60), ppO2, 4.57)
-        self.cns += self.TI(time, ppO2, 6.8) / 26.108
+        P_amb: Pressure = Pressure.from_depth(depth)
+        gas.consume(P_amb, time)
